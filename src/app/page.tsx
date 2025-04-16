4f838f09190007a0057ce2d46e1817d31f984432
Evaldo Souza <evaldo.souza2@gmail.com>
2025-04-16 13:11:37 -0300

    Fazendo isso ser a pagina inicial do si3cloud

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>SUB DOMINIO!!!!</h1>
+      <h1>Pagina inicial do si3cloud</h1>
+      <Link href={"http://si3cloud.ufsj.edu.br/riasec360"}>RIASEC360</Link>
+      <Link href={"/extra"}>Pagina de teste neste projeto</Link>
     </div>
   );
 }
